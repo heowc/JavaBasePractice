e21e6f5e2e5d17f2029d49d5f4e2dccf83e83b00
heo won chul <heowc1992@gmail.com>
2017-04-01 20:49:52 +0900

    refactoring cached thread & remove test package

--- ThreadPool/src/com/tistory/heowc/threadpool/CachedThreadApplication.java
+++ ThreadPool/src/com/tistory/heowc/threadpool/CachedThreadApplication.java
@@ -1,24 +1,45 @@
 package com.tistory.heowc.threadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class CachedThreadApplication {
 
-    public static void main(String [] args) {
-        ExecutorService threadPoolExecutor = Executors.newCachedThreadPool();
+    public static void main(String [] args) throws InterruptedException {
+        System.out.println("================ getFairIsTrueCachedExecutorService");
+        ExecutorService executorService1 = getFairIsTrueCachedExecutorService();
+        printExecutorService(executorService1);
 
-        IntStream.range(0, 1000)
-                .forEach(action -> threadPoolExecutor.execute(() -> {
-                    try {
-                        Thread.sleep(1000L);
-                        System.out.println(threadPoolExecutor.toString());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }));
+        Thread.sleep(3000L);
 
+        System.out.println("================ getFairIsFalseCachedExecutorService");
+        ExecutorService executorService2 = getFairIsFalseCachedExecutorService();
+        printExecutorService(executorService2);
+    }
+
+    private static ExecutorService getFairIsTrueCachedExecutorService() {
+        return Executors.newCachedThreadPool();
+
+    }
+
+    private static ExecutorService getFairIsFalseCachedExecutorService() {
+        return new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                                        60L, TimeUnit.SECONDS,
+                                        new SynchronousQueue<Runnable>(true));
+    }
+
+    private static void printExecutorService(ExecutorService executorService) {
+        IntStream.range(0, 10)
+                .mapToObj(value -> executorService.submit(() -> value))
+                .forEach(action -> {
+                    executorService.submit(() -> {
+                        try {
+                            System.out.println(executorService.toString());
+                            System.out.println(action.get());
+                        } catch (InterruptedException | ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                });
     }
 }
