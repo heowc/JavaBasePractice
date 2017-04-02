fa7417696dfdff67d62fb83d57d85fee87027456
heo won chul <heowc1992@gmail.com>
2017-04-02 15:40:06 +0900

    refactoring synchronous queue

--- BlockingQueue/test/com/tistory/heowc/blockingqueue/SynchronousQueueTests.java
+++ BlockingQueue/test/com/tistory/heowc/blockingqueue/SynchronousQueueTests.java
@@ -1,6 +1,8 @@
 package com.tistory.heowc.blockingqueue;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.concurrent.SynchronousQueue;
 
@@ -11,5 +13,11 @@ public class SynchronousQueueTests {
     @Before
     public void before_init() {
         queue = new SynchronousQueue<>(); // default is false
+    }
+
+    @Test
+    public void test_offer() throws Exception {
+        // This queue does not have buffer
+        Assert.assertFalse("Queue full", queue.offer(1));
     }
 }
