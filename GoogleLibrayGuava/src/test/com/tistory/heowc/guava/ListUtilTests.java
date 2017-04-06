8253529bad1abad99626bee714716283b8975b50
heowonchul <heowc1992@gmail.com>
2017-04-06 11:22:58 +0900

    modify guava collection

--- GoogleLibrayGuava/src/test/com/tistory/heowc/guava/ListUtilTests.java
+++ GoogleLibrayGuava/src/test/com/tistory/heowc/guava/ListUtilTests.java
@@ -3,13 +3,26 @@ package com.tistory.heowc.guava;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.partitioningBy;
 
 public class ListUtilTests {
 
     @Test
     public void test_newArrayList() {
         Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9).forEach(System.out::println);
+
+        // JDK 6 이하
+        List<Integer> list1 = new ArrayList<Integer>();
+
+        // JDK 7 이상
+        List<Integer> list2 = new ArrayList<>();
+
+        // Guava
+        List<Integer> list3 = Lists.newArrayList();
     }
 
     @Test
@@ -21,13 +34,29 @@ public class ListUtilTests {
     @Test
     public void test_partitionToLists() {
         Integer groupSize = 3;
+
         List<Integer> numberList = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
+        // JDK 8
+        Map<Boolean, List<Integer>> groupMap = numberList.stream()
+                .collect(partitioningBy(i -> i > 5));
+        Lists.newArrayList(groupMap.values())
+            .forEach(list -> {
+                list.forEach(System.out::print);
+                System.out.println();
+            });
+        // 1234
+        // 56789
+
+        // Guava
         Lists.partition(numberList, groupSize)
-                .forEach(list -> {
-                    list.forEach(System.out::print);
-                    System.out.println();
-                });
+            .forEach(list -> {
+                list.forEach(System.out::print);
+                System.out.println();
+            });
+        // 123
+        // 456
+        // 789
     }
 
     @Test
