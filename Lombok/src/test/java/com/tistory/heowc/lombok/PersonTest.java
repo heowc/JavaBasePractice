faba264abef8f8f7d47d3575222750398a814bc8
heo won chul <heowc1992@gmail.com>
2017-06-01 21:17:18 +0900

    add log

--- Lombok/src/test/java/com/tistory/heowc/lombok/PersonTest.java
+++ Lombok/src/test/java/com/tistory/heowc/lombok/PersonTest.java
@@ -1,5 +1,6 @@
 package com.tistory.heowc.lombok;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,10 +8,12 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 public class PersonTest {
 
     @Test
     public void test_equalAndHashCode() throws Exception {
+        log.info("test_equalAndHashCode");
         Person person = new Person();
         person.setFirstName("won chul");
         person.setLastName("heo");
@@ -21,6 +24,7 @@ public class PersonTest {
 
     @Test
     public void test_builder() throws Exception {
+        log.info("test_builder");
         Person person = Person.of("won chul",
                                     "heo",
                                     LocalDate.of(1992, 7, 8));
@@ -29,6 +33,7 @@ public class PersonTest {
 
     @Test
     public void test_hashSet() throws Exception {
+        log.info("test_hashSet");
         Set<Person> personSet = new HashSet<>();
         personSet.add(Person.getDefaultPerson());
         personSet.add(Person.getDefaultPerson());
