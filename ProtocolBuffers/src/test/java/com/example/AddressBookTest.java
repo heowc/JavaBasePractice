ac1386d9aca4da43339c1cf14e1d4814331ea06e
heowc <heowc1992@gmail.com>
2019-06-03 23:50:47 +0900

    [fix] protocol buffers

--- ProtocolBuffers/src/test/java/com/example/AddressBookTest.java
+++ ProtocolBuffers/src/test/java/com/example/AddressBookTest.java
@@ -1,15 +1,17 @@
 package com.example;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AddressBookTest {
 
-    @Test
-    public void test() {
+    private AddressBookProtos.AddressBook getAddressBook() {
         AddressBookProtos.Person john =
                 AddressBookProtos.Person.newBuilder()
                         .setId(1234)
@@ -21,8 +23,10 @@ public class AddressBookTest {
                                         .setType(AddressBookProtos.Person.PhoneType.HOME))
                         .build();
 
-        AddressBookProtos.AddressBook addressBook = AddressBookProtos.AddressBook.newBuilder().addPeople(john).buildPartial();
+        return AddressBookProtos.AddressBook.newBuilder().addPeople(john).build();
+    }
 
+    private void assertThatAll(AddressBookProtos.AddressBook addressBook) {
         assertThat(addressBook.getPeopleList().size(), is(1));
         assertThat(addressBook.getPeopleList().get(0).getId(), is(1234));
         assertThat(addressBook.getPeopleList().get(0).getName(), is("John Doe"));
@@ -31,4 +35,25 @@ public class AddressBookTest {
         assertThat(addressBook.getPeopleList().get(0).getPhones(0).getNumber(), is("555-4321"));
         assertThat(addressBook.getPeopleList().get(0).getPhones(0).getTypeValue(), is(AddressBookProtos.Person.PhoneType.HOME.getNumber()));
     }
+
+    @Test
+    public void simple() {
+        AddressBookProtos.AddressBook addressBook = getAddressBook();
+        assertThatAll(addressBook);
+    }
+
+    @Test
+    public void writeAndRead() throws IOException {
+        AddressBookProtos.AddressBook outputAddressBook = getAddressBook();
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        outputAddressBook.writeTo(output);
+        output.close();
+
+        ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
+        AddressBookProtos.AddressBook inputAddressBook = AddressBookProtos.AddressBook.parseFrom(input);
+
+        assertThatAll(inputAddressBook);
+    }
+
 }
