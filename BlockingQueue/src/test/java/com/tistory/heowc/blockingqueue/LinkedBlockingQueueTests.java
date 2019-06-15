513a3354b7c6c6079c0b0965cb7d4b7e3cf580d1
heowc <heowc1992@gmail.com>
2019-06-15 14:37:58 +0900

    [fix] add test code for drainTo from multi thread situation

--- BlockingQueue/src/test/java/com/tistory/heowc/blockingqueue/LinkedBlockingQueueTests.java
+++ BlockingQueue/src/test/java/com/tistory/heowc/blockingqueue/LinkedBlockingQueueTests.java
@@ -4,7 +4,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class LinkedBlockingQueueTests {
@@ -23,5 +26,36 @@ public class LinkedBlockingQueueTests {
 
         Assert.assertEquals("Queue Remain Capacity is ( 2^31 - 11 )",
                 queue.remainingCapacity(), Integer.MAX_VALUE - 10);
+        queue.clear();
+    }
+
+    @Test
+    public void test_drainToFromMultiThread() {
+        CountDownLatch countDownLatch = new CountDownLatch(100);
+
+        ExecutorService putPool = Executors.newFixedThreadPool(2);
+        ExecutorService takePool = Executors.newFixedThreadPool(1);
+
+        for (int i = 0; i < 100; i++) {
+            putPool.execute(() -> {
+                int number = ThreadLocalRandom.current().nextInt(10);
+                System.out.println("inserted: " + number);
+                queue.offer(number);
+                countDownLatch.countDown();
+            });
+        }
+
+        while (countDownLatch.getCount() != 0 || !queue.isEmpty()) {
+            takePool.execute(() -> {
+                if (queue.size() >= 10) {
+                    System.out.println(putPool);
+                    List<Integer> list = new ArrayList<>();
+                    queue.drainTo(list, 10);
+                    System.out.println(Arrays.toString(list.toArray()));
+                }
+            });
+        }
+
+        System.out.println("remain size = " + queue.size());
     }
 }
